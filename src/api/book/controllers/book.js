c211994ad85000b151a9b73c7e92dc766af703d0
psu6510110233 <6510110233@email.psu.ac.th>
2023-02-15 12:12:49 +0700

    strapi

---
+++ src/api/book/controllers/book.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * book controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+import {factories}
+
+module.exports = createCoreController('api::book.book', ({ strapi }) => ({
+
+}));
